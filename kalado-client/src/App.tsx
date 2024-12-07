6ccb239f3d2677a51620067422599ab3ad60c08d
Fonij80 <forooz.iraji@gmail.com>
2024-12-07 13:44:09 +0330

    add CodeVerification component

--- kalado-client/src/App.tsx
+++ kalado-client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import CodeVerification from './components/Signup/CodeVerification'
 
 function App() {
   const [isSignupOpen, setSignupOpen] = useState(false);
@@ -14,6 +15,12 @@ function App() {
 
   return (
     <div>
+      <nav>
+        <button onClick={handleSignupClick}>Sign Up</button>
+      </nav>
+      {isSignupOpen && (
+        <CodeVerification onClose={handleCloseSignup} email="f@gmail.com" />
+      )}
     </div>
   )
 }
