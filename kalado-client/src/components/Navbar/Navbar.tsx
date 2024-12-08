c659509a362985bd44e1dcf102543658d1290b72
DnyaNvB <nvbdnya@gmail.com>
2024-12-08 11:28:07 +0330

    add Navigation bar

---
+++ kalado-client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Navbar.css';
+import { FaSearch } from 'react-icons/fa';
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="page-container">
+      <nav className="navbar">
+        {/* Logo */}
+        <div className="navbar-logo">کالادو</div>
+
+        {/* Search Bar */}
+        <div className="navbar-search">
+          <input type="text" placeholder="جستجوی کالا" />
+          <button>
+            <FaSearch />
+          </button>
+        </div>
+
+        {/* Buttons */}
+        <div className="navbar-buttons">
+          <button className="navbar-button signup">ثبت آگهی</button>
+          <button className="navbar-button login">ورود/ثبت‌نام</button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
