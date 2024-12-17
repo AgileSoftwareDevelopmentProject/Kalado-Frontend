312518090ef63f0f73a31f91c903afde9f95472a
Fonij80 <forooz.iraji@gmail.com>
2024-12-17 21:41:09 +0330

    add validatePassword

--- kalado-client/src/validators/validatePassword.ts
+++ kalado-client/src/validators/validatePassword.ts
@@ -6,6 +6,10 @@ export const validatePassword = (password: string, t: TFunction) => {
     const hasUppercase = /[A-Z]/;
     const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/;
 
+    if (password === '') {
+        return { valid: true, error: '' };
+    }
+
     if (password.length < minLength) {
         return { valid: false, error: t("error.input.invalid_password.min_length") };
     }
