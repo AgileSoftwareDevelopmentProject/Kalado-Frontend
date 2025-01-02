9e6e62ee852898d665d2c8a7458db94011a6cc25
amirsaeedahmadi <amirsaeedahmadi80@yahoo.com>
2025-01-02 21:39:35 +0330

    fix: a minor bug in email verification api and refactoring services apis

---
+++ kalado-client/src/services/product/DeleteAdService.ts
@@ -0,0 +1,23 @@
+import { sendRequest } from '../../axiosInstance';
+import { PRODUCT } from '../../urls';
+
+export async function deleteAd(
+    id: number // for the ad to delete
+) {
+    try {
+        const response = await sendRequest(`${PRODUCT.DELETE}/${id}`, 'DELETE');
+
+        if (response.isSuccess) {
+            console.log('Ad deleted successfully!');
+        } else if (response.status === 400) {
+            console.log('Bad Request. Please check the input data.');
+        } else {
+            console.log(response.message || 'Ad deletion failed.');
+        }
+
+        return response;
+    } catch (error) {
+        console.error('An unexpected error occurred during ad deletion.', error);
+        return { isSuccess: false, message: 'An error occurred during ad deletion.' };
+    }
+}
