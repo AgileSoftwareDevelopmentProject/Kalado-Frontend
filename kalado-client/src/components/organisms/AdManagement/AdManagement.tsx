217b0c55bd91d0d1ae2103e39aed2eff8b3d1121
DnyaNvB <nvbdnya@gmail.com>
2025-01-03 17:49:27 +0330

    style(admanagement): center AdList component on the page

--- kalado-client/src/components/organisms/AdManagement/AdManagement.tsx
+++ kalado-client/src/components/organisms/AdManagement/AdManagement.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import AdList from '../AdList/AdList';
+import EditAdCard from '../AdCard/EditAdCard'; // Import EditAdCard
 
 interface AdData {
     id: number;
@@ -16,12 +17,34 @@ interface AdData {
 interface AdManagementProps {
     ads: AdData[]; // Include ads as a prop
     onEdit: (adData: AdData) => void; // Enforce type of adData
+    selectedAd?: AdData;
+    onCloseEdit: () => void;
 }
 
-const AdManagement: React.FC<AdManagementProps> = ({ ads, onEdit }) => {
+const AdManagement: React.FC<AdManagementProps> = ({ ads, onEdit, selectedAd, onCloseEdit }) => {
     return (
-        <Box>
-            <AdList ads={ads} onEdit={onEdit} />
+        <Box sx={{ padding: 15}}>
+            {/* Ad List */}
+            <Box sx={{ marginBottom: 4 }}>
+                <AdList ads={ads} onEdit={onEdit} />
+            </Box>
+
+            {/* Edit Ad Card */}
+            {selectedAd && (
+                <Box sx={{ marginTop: 4 }}>
+                    <EditAdCard
+                        title={selectedAd.title}
+                        price={selectedAd.price}
+                        category={selectedAd.category}
+                        date={selectedAd.date}
+                        description={selectedAd.description}
+                        images={selectedAd.images}
+                        status={selectedAd.status}
+                        onEdit={(updatedData) => onEdit(updatedData)}
+                        onCancel={onCloseEdit}
+                    />
+                </Box>
+            )}
         </Box>
     );
 };
