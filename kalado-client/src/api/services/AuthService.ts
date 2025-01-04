b734065084c18e2cb76f4027c859e70f2baaf0fa
amirsaeedahmadi <amirsaeedahmadi80@yahoo.com>
2025-01-05 01:10:03 +0330

    fix: minor bug

--- kalado-client/src/api/services/AuthService.ts
+++ kalado-client/src/api/services/AuthService.ts
@@ -38,3 +38,14 @@ export async function signupUser(userData: UserData) {
         }
     );
 }
+
+
+export async function verifyCode(token: string) {
+    return sendRequest(
+        AUTH.VERIFY,
+        'POST',
+        new URLSearchParams({ token }).toString(),
+        undefined,
+        { 'Content-Type': 'application/x-www-form-urlencoded' } 
+    );
+}
