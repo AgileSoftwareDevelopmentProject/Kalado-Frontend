b2e593e1a513c68ee58123ed06d7cb54908cedc3
DnyaNvB <nvbdnya@gmail.com>
2025-01-08 02:31:59 +0330

    fix: axios

--- kalado-client/src/api/services/axiosInstance.ts
+++ kalado-client/src/api/services/axiosInstance.ts
@@ -2,16 +2,15 @@ import axios from 'axios';
 import { useAuth } from '../contexts/AuthContext';
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL || 'http://kaladoshop.com:8081', // Ensure your base URL is correct
+    baseURL: 'http://kaladoshop.com:8081',
     headers: {
         'Content-Type': 'application/json',
     },
 });
 
-// Add interceptors to attach the token dynamically
 instance.interceptors.request.use(
     (config) => {
-        const token = localStorage.getItem('token'); // Retrieve token from localStorage
+        const token = localStorage.getItem('token');
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
