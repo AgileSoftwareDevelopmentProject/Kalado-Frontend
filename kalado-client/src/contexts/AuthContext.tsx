8e1ede226683927200712627ece4f85ec29ae826
Fonij80 <forooz.iraji@gmail.com>
2025-01-21 22:33:50 +0330

    edit role deafult value

--- kalado-client/src/contexts/AuthContext.tsx
+++ kalado-client/src/contexts/AuthContext.tsx
@@ -11,7 +11,7 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [token, setToken] = useState<string | null>(null);
-    const [role, setRole] = useState<string | null>(null);
+    const [role, setRole] = useState<string | null>('USER');
 
     return (
         <AuthContext.Provider value={{ token, role, setToken, setRole }}>
