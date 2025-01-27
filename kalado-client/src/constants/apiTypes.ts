6c2f4906c428ae09056f872e0cf433f925eaa4ab
amirsaeed ahmadi <amirsaeedahmadi80@yahoo.com>
2025-01-28 02:28:02 +0330

    feat: change Product Data type

--- kalado-client/src/constants/apiTypes.ts
+++ kalado-client/src/constants/apiTypes.ts
@@ -68,7 +68,7 @@ export interface ProductData {
     };
     category: string;
     images?: File[] | null;
-    productionYear?: Date | null;
+    productionYear?: number | null;
     brand?: string | null;
     // sellerId: number;
 }
