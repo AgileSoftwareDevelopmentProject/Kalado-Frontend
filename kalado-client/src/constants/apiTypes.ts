fa94f3d6916bbdbc3e83822ff8690cd5af2cc225
amirsaeed ahmadi <amirsaeedahmadi80@yahoo.com>
2025-01-28 03:05:17 +0330

    fix: TProductResponseType

--- kalado-client/src/constants/apiTypes.ts
+++ kalado-client/src/constants/apiTypes.ts
@@ -76,7 +76,7 @@ export interface ProductData {
 export type TProductResponseType = {
     id: number;
     title: string;
-    createdAt: string;
+    createdAt: number;
     imageUrls?: string[];
     price: {
         amount: number,
@@ -86,7 +86,7 @@ export type TProductResponseType = {
     sellerPhoneNumber: string;
     sellerId: number;
     brand?: string;
-    productionYear?: string;
+    productionYear?: number;
     status: string;
 };
 
