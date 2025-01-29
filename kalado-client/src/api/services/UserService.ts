69f4a30c3f6932268743564efed3577b2cb7bd99
amirsaeed ahmadi <amirsaeedahmadi80@yahoo.com>
2025-01-29 15:10:55 +0330

    fix: minor changes

--- kalado-client/src/api/services/UserService.ts
+++ kalado-client/src/api/services/UserService.ts
@@ -12,8 +12,26 @@ export async function getProfile() {
     );
 }
 
-export async function modifyProfile(profileData: ProfileData) {
-    return sendRequest(
+// export async function modifyProfile(profileData: ProfileData) {
+//     return sendRequest(
+//         USER.MODIFY_PROFILE,
+//         'POST',
+//         profileData,
+//         {},
+//         'multipart/form-data'
+//     );
+// }
+
+export async function modifyProfile(profileData: ProfileData, imageFile: File) {
+    const formData = new FormData();
+    formData.append('profile', JSON.stringify(profileData));
+
+    // imageFiles.forEach((file, index) => {
+    //     formData.append(`profileImage[${index}]`, file);
+    // });
+    formData.append(`profileImage`, imageFile);
+
+    return sendRequest<TUserProfileResponse>(
         USER.MODIFY_PROFILE,
         'POST',
         profileData,
