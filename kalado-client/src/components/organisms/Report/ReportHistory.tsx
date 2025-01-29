f9a6ee1c8c1b0b06b11601fda1aabbe8bccb93af
Fonij80 <forooz.iraji@gmail.com>
2025-01-29 14:03:49 +0330

    refactor: refactor ReportHistory

--- kalado-client/src/components/organisms/Report/ReportHistory.tsx
+++ kalado-client/src/components/organisms/Report/ReportHistory.tsx
@@ -37,78 +37,78 @@ const ReportHistory: React.FC<ReportHistoryProps> = ({ reportsList }) => {
                 textAlign: isRtl ? 'right' : 'left',
             }}
         >
-            {error && <FormError message={error} />}
 
-            {/* {!reports ? (
-                <>
-                    <Grid container spacing={3} justifyContent="center">
-                        {reports.length > 0 ? (
-                            reports.map((report) => (
-                                <Grid item xs={12} sm={6} md={4} key={report.id}>
-                                    <Card
+            {reports ? (
+                <Grid container spacing={3} justifyContent="center">
+                    {reports.length > 0 ? (
+                        reports.map((report) => (
+                            <Grid item xs={12} sm={6} md={4} key={report.id}>
+                                <Card
+                                    sx={{
+                                        padding: 2,
+                                        boxShadow: '0px 2px 10px rgba(0,0,0,0.1)',
+                                        borderRadius: '15px',
+                                        margin: '0 auto',
+                                    }}
+                                >
+                                    <Typography variant="h6" sx={{ marginBottom: 1 }}>
+                                        {t('report.report_card.violation_type')}: {report.violationType}
+                                    </Typography>
+                                    <Typography variant="body2" sx={{ marginBottom: 1 }}>
+                                        {t('report.report_card.reporter_id')}: {report.reporterId}
+                                    </Typography>
+                                    <Typography variant="body2" sx={{ marginBottom: 1 }}>
+                                        {t('report.report_card.content_id')}: {report.reportedContentId}
+                                    </Typography>
+                                    <Typography variant="body2" sx={{ marginBottom: 1 }}>
+                                        {t('general_inputs.date')}: {new Intl.DateTimeFormat('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }).format(
+                                            new Date(
+                                                report.createdAt[0], // Year
+                                                report.createdAt[1] - 1, // Month (0-based index)
+                                                report.createdAt[2] // Day
+                                            )
+                                        )}
+                                    </Typography>
+                                    <Button
+                                        variant="text"
+                                        onClick={() => handleShowDetails(report)}
                                         sx={{
-                                            padding: 2,
-                                            boxShadow: '0px 2px 10px rgba(0,0,0,0.1)',
-                                            borderRadius: '15px',
-                                            margin: '0 auto',
+                                            display: 'flex',
+                                            alignItems: 'center',
+                                            marginTop: 2,
+                                            textTransform: 'none',
+                                            justifyContent: isRtl ? 'flex-end' : 'flex-start',
                                         }}
                                     >
-                                        <Typography variant="h6" sx={{ marginBottom: 1 }}>
-                                            {t('report.report_card.violation_type')}: {report.violationType}
-                                        </Typography>
-                                        <Typography variant="body2" sx={{ marginBottom: 1 }}>
-                                            {t('report.report_card.reporter_id')}: {report.reporterId}
-                                        </Typography>
-                                        <Typography variant="body2" sx={{ marginBottom: 1 }}>
-                                            {t('report.report_card.content_id')}: {report.reportedContentId}
-                                        </Typography>
-                                        <Typography variant="body2" sx={{ marginBottom: 1 }}>
-                                            {t('general_inputs.date')}: {new Intl.DateTimeFormat('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }).format(
-                                                new Date(
-                                                    report.createdAt[0], // Year
-                                                    report.createdAt[1] - 1, // Month (0-based index)
-                                                    report.createdAt[2] // Day
-                                                )
-                                            )}
-                                        </Typography>
-                                        <Button
-                                            variant="text"
-                                            onClick={() => handleShowDetails(report)}
-                                            sx={{
-                                                display: 'flex',
-                                                alignItems: 'center',
-                                                marginTop: 2,
-                                                textTransform: 'none',
-                                                justifyContent: isRtl ? 'flex-end' : 'flex-start',
-                                            }}
-                                        >
-                                            <InfoOutlinedIcon sx={{ marginRight: isRtl ? 0 : 1, marginLeft: isRtl ? 1 : 0 }} />
-                                            {t('report.report_card.actions.show_details')}
-                                        </Button>
-                                    </Card>
-                                </Grid>
-                            ))
-                        ) : (
-                            <Typography variant="body1" sx={{ textAlign: 'center', marginTop: 4 }}>
-                                {t('report.report_card.no_reports')}
-                            </Typography>
-                        )}
-                    </Grid>
-                </>
+                                        <InfoOutlinedIcon sx={{ marginRight: isRtl ? 0 : 1, marginLeft: isRtl ? 1 : 0 }} />
+                                        {t('report.report_card.actions.show_details')}
+                                    </Button>
+                                </Card>
+                            </Grid>
+                        ))
+                    ) : (
+                        <Typography variant="body1" sx={{ textAlign: 'center', marginTop: 4 }}>
+                            {t('report.report_card.no_reports')}
+                        </Typography>
+                    )}
+                </Grid>
             ) : (
-                <ReportDetails
-                    report={{
-                        violationType: selectedReport.violationType,
-                        description: selectedReport.description,
-                        image: selectedReport.evidenceFiles,
-                        contentId: selectedReport.reportedContentId,
-                    }}
-                    onBackToList={handleBackToList}
-                    onBlockContent={(contentId) => {
-                        console.log('Block content with ID:', contentId);
-                    }}
-                />
-            )} */}
+                <FormError message={error} />
+            )}
+
+
+            {/* <ReportDetails
+                report={{
+                    violationType: selectedReport.violationType,
+                    description: selectedReport.description,
+                    image: selectedReport.evidenceFiles,
+                    contentId: selectedReport.reportedContentId,
+                }}
+                onBackToList={handleBackToList}
+                onBlockContent={(contentId) => {
+                    console.log('Block content with ID:', contentId);
+                }}
+            /> */}
         </Box>
     );
 };
